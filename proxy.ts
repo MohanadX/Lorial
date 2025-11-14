858f5fda8d6a04c49be249142e463c9b51f82284
Mohanad Ayoub <alaqlmhnd3@gmail.com>
2025-11-14 15:18:57 +0300

    removed auth from proxy because it depend on node only package can't work on edge runtime

--- proxy.ts
+++ proxy.ts
@@ -1,38 +1,35 @@
 import { NextRequest, NextResponse } from "next/server";
-import { auth } from "./auth";
+import { getToken } from "next-auth/jwt"; //does not require Node modules; it reads the JWT from cookies and works in Edge.
 
 export const config = {
 	matcher: ["/event/create/:path*", "/user/:path*", "/login"],
 };
 
-export default async function proxy(req: NextRequest) {
-	const session = await auth();
-	const isLogged = session?.user;
+export default async function middleware(req: NextRequest) {
+	const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+	const isLogged = !!token;
 	const pathname = req.nextUrl.pathname;
+
 	// 1) Block logged-in users from accessing /login
 	if (pathname === "/login" && isLogged) {
 		return NextResponse.redirect(new URL("/", req.url));
 	}
 
-	//  2) Protect /event/create/*
+	// 2) Protect /event/create/*
 	if (pathname.startsWith("/event/create") && !isLogged) {
 		return NextResponse.redirect(new URL("/", req.url));
 	}
 
-	// 3) protect other users pages from guessing their id and prevent non-logged users to reach it
+	// 3) Protect /user/:id pages
 	if (pathname.startsWith("/user/")) {
 		const requestUserId = pathname.split("/")[2] ?? null;
-		const userId = isLogged?.id;
 		if (!isLogged) {
 			return NextResponse.redirect(new URL("/login", req.url));
 		}
-
-		// Prevent access to other users’ pages
-		if (requestUserId !== userId) {
+		if (requestUserId !== token.id) {
 			return NextResponse.redirect(new URL("/unauthorized", req.url));
 		}
 	}
 
-	// Authentication passed — continue request
 	return NextResponse.next();
 }
